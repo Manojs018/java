8b03f86e3941e23aca044a271e70fb1c443fa954
Manojs018 <>
2026-02-10 12:18:02 +0530

    user input example

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Input {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter you roll no ");
+        int roll = input.nextInt();
+        System.out.println("your roll number is " + roll);
+
+    }
+}
