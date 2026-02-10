5e0c52eaed5f59fdf00e1d21d5be85a35771d46f
Manojs018 <>
2026-02-10 12:23:06 +0530

    Temperature in celcious to farenheat

---
+++ Tem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Tem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the temperature in C ");
+        float tempC = input.nextFloat();
+        float tempF = (tempC * 9/5) +32;
+        System.out.println("Temperature in F " + tempF);
+        
+    }
+}
