35e67948ca8c4ba7370318fcbb4338106662c794
Manojs018 <>
2026-02-10 12:10:45 +0530

    data types

---
+++ Premitives.java
@@ -0,0 +1,11 @@
+public class Premitives {
+    public static void main(String[] args) {
+        int rollno = 54;
+        char letter = 'A';
+        float dec =12.45f;
+        double longerdecimal = 45.12345677;
+        long longinteger = 123456789L;
+
+
+    }
+}
