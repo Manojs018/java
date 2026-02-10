963890ed2f7f4db3b7245a6fe47104935846f657
Manojs018 <>
2026-02-10 14:51:16 +0530

    Simple Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ans = 0;
+        System.out.print("Enter first operator");
+        char op = input.next().trim().charAt(0);
+        while (true) {
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter the first number");
+                int num1 = input.nextInt();
+                System.out.print("Enter the second number");
+                int num2 = input.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num2 / num1;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+
+            } else {
+                System.out.println("Invalid operator");
+            }
+            System.out.println(ans);
+        }
+    }
 }
