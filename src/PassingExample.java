fbdf07474b66c89160ba91f6b463a1c7a476d597
Manojs018 <>
2026-02-12 11:31:42 +0530

    Modified value

---
+++ src/PassingExample.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        printArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void printArray(int[] num) {
+        num[0] = 18;
+
+    }
+}
